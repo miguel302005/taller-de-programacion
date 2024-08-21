16a8b1a5f27b921fc8034d570b6f070c003ab5b7
miguel302005 <rumrumperez@gmail.com>
2024-08-21 10:30:21 -0500

    mi primer commit

---
+++ js/demo1.js
@@ -0,0 +1,9 @@
+// funcion suma 
+// es una funcion sin parametros 
+function suma(){
+let numero = 13;
+let numero1 = 32;
+let resultado = numero+numero1;
+console.log("las suma es :"+resultado)
+}
+suma();
